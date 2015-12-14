8dd8e29842003f1ccdda4a7123e8a747dbb00532
CM <m@gmail.com>
2015-12-14 13:40:38 +0200

    Edni zadachi

---
+++ Call.java
@@ -0,0 +1,8 @@
+package package_GSM$E;
+
+public class Call {
+	double priceForAMinute = 0.2;
+	GSM caller;
+	GSM receiver;
+	int duration;
+}
---
+++ GSMdemo.java
@@ -0,0 +1,24 @@
+package package_GSM$E;
+
+
+public class GSMdemo {
+	public static void main(String[] args) {
+		GSM motorola = new GSM();
+		GSM nokia = new GSM();
+		
+		nokia.isValid.validSIMcard( String simMobileNumber,boolean hasSimCard);
+		
+		nokia.insertSimCard("0883131929");
+		motorola.insertSimCard("0889342546");
+		
+//		nokia.removeSimCard();
+		
+		nokia.call(motorola, 2);
+		
+		nokia.printInfoForLastOutgoingCall();
+
+		motorola.printInfoForLastIncomingCall();
+		
+		System.out.println(nokia.getSumForCalls());
+	}
+}
